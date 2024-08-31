faf243d397060a1747549ad890117d64048a6802
Xraww <xrw3008@gmail.com>
2024-08-31 19:28:57 +0200

    Responsive components: Projects, Skills & Contact. Body background

--- client/src/components/Projects/Projects.jsx
+++ client/src/components/Projects/Projects.jsx
@@ -16,7 +16,7 @@ function Projects() {
         }
     };
 
-    const handleGSAPAnimation = (closeIconRef, imageRef, textRef, clickIconRef, showText) => {
+    const handleGSAPAnimation = (titleRef, closeIconRef, imageRef, textRef, clickIconRef, showText) => {
         if (imageRef.current && textRef.current) {
             if (showText) {
                 gsap.to(imageRef.current, { opacity: 0, scale: 0.8, duration: 0.5 });
@@ -24,6 +24,7 @@ function Projects() {
                 textRef.current.style.pointerEvents = "auto";
                 imageRef.current.style.pointerEvents = "none";
 
+                titleRef.current.style.display = "none";
                 closeIconRef.current.style.display = "block";
                 clickIconRef.current.style.color = "transparent";
             } else {
@@ -32,26 +33,37 @@ function Projects() {
                 textRef.current.style.pointerEvents = "none";
                 imageRef.current.style.pointerEvents = "auto";
 
+                titleRef.current.style.display = "block";
                 closeIconRef.current.style.display = "none";
                 clickIconRef.current.style.color = "black";
             }
         }
     };
 
-    const createParallaxLayer = (offset, title, imageSrc, text, link, scrollLeft, scrollRight) => {
+    const projectsText = [
+        {description: "Création de la page d'accueil d'une agence de voyage avec HTML & CSS", problems: "Conception responsive", overcome: "Utilisation de media queries et flexbox", newSkills: "HTML / CSS / Responsive / Git"},
+        {description: "Création d'une page web dynamique avec JavaScript", problems: "Gestion des événements et interactions utilisateurs. Manipulation du DOM", overcome: "Utilisation de addEventListener. Fonction modulaire pour gérer les différentes interactions. Cela a permis de maintenir un code propre et facilement déboguable.", newSkills: "Javascript / Modale / Call API (fetch) / NPM / "},
+        {description: "Création d'une application web de location immobilière avec React", problems: "Gestion de l'état global et navigation fluide", overcome: "Utilisation de React hooks comme useState et useEffect. Configuration de React Router pour gérer les routes et la navigation.", newSkills: "React / React Router / Animation CSS / Sass (scss) / CRA / Vite"},
+        {description: "Développement du back-end d'un site de notation de livres", problems: "Conception de la base de données & Sécurisation de l'API", overcome: "Utilisation de JWT pour l'authentification des utilisateurs. J'ai également utilisé des pratiques comme le chiffrement des mots de passe avec bcrypt.", newSkills: "Node.js / Express / MongoDB / Mongoose / API RESTful"},
+        {description: "Débogage et optimisation du site d'une photographe", problems: "Débogage complexe. Optimisation du référencement (SEO). Performance et vitesse de chargement", overcome: "Utilisation d'outils de débogage et de statistiques comme DevTools et Lighthouse. Utilisation de balises sémantiques, de balises meta et de microdata Schema.org.", newSkills: "Déboguage / Optimisation de référencement (SEO) / Lighthouse / Optimisation d'image / Minification de fichiers"},
+    ]
+
+    const createParallaxLayer = (offset, title, imageSrc, link, scrollLeft, scrollRight) => {
         const [showText, setShowText] = useState(false);
+        const titleRef = useRef(null);
         const closeIconRef = useRef(null);
         const imageRef = useRef(null);
         const textRef = useRef(null);
         const clickIconRef = useRef(null);
     
         useEffect(() => {
-            handleGSAPAnimation(closeIconRef, imageRef, textRef, clickIconRef, showText);
+            handleGSAPAnimation(titleRef, closeIconRef, imageRef, textRef, clickIconRef, showText);
         }, [showText]);
+
         return (
             <ParallaxLayer offset={offset} speed={0.5} className="layers">
                 <div className="content">
-                <button className="back-button" ref={closeIconRef} onClick={() => setShowText(false)}><FontAwesomeIcon icon={faXmark}/></button>
+                    <button className="back-button" ref={closeIconRef} onClick={() => setShowText(false)}><FontAwesomeIcon icon={faXmark}/></button>
 
                     <div className="project-page">
                         <div className="image-icon">
@@ -59,11 +71,21 @@ function Projects() {
                             <span ref={clickIconRef} id="click-icon"><FontAwesomeIcon icon={faHandPointer}/></span>
                         </div>
 
-                        <h3>Projet {offset + 1} - {title}</h3>
+                        <h3 ref={titleRef}>Projet {offset + 1} - {title}</h3>
 
                         <div ref={textRef} className="project-text">
-                            <h2>Détails du projet</h2>
-                            <p>{text}</p>
+                            <div className="titles">
+                                <h2>Détails du projet</h2>
+                                <h4>Projet {offset + 1} - {title}</h4>
+                            </div>
+
+                            <div className="paragraphs">
+                                <p><span>Description</span><span className="paragraphs-text">{projectsText[offset].description}</span></p>
+                                <p><span>Problèmes rencontrées</span><span className="paragraphs-text">{projectsText[offset].problems}</span></p>
+                                <p><span>Comment je les ai surmontées</span><span className="paragraphs-text">{projectsText[offset].overcome}</span></p>
+                                <p><span>Compétences développées</span><span className="paragraphs-text">{projectsText[offset].newSkills}</span></p>
+                            </div>
+
                             <a href={link} target="_blank">Lien vers le répository GitHub</a>
                         </div>
                     </div>
@@ -81,12 +103,12 @@ function Projects() {
 
             <div className="parallax">
                 <Parallax pages={5} ref={parallax} horizontal>
-                    {createParallaxLayer(0, "Booki", "/images/project-1.png", "Lorem ipsum dolor sit amet, consectetur adipisicing elit.", "https://github.com/Xraww/Booki", 4, 1)}
-                    {createParallaxLayer(1, "Sophie Bluel | Portfolio", "/images/project-2.png", "Lorem ipsum dolor sit amet, consectetur adipisicing elit.", "https://github.com/Xraww/Architecte", 0, 2)}
-                    {createParallaxLayer(2, "Kasa Location", "/images/project-3.png", "Lorem ipsum dolor sit amet, consectetur adipisicing elit.","https://github.com/Xraww/Kasa", 1, 3)}
+                    {createParallaxLayer(0, "Booki", "/images/project-1.png", "https://github.com/Xraww/Booki", 4, 1)}
+                    {createParallaxLayer(1, "Sophie Bluel | Portfolio", "/images/project-2.png", "https://github.com/Xraww/Architecte", 0, 2)}
+                    {createParallaxLayer(2, "Kasa Location", "/images/project-3.png", "https://github.com/Xraww/Kasa", 1, 3)}
 
-                    {createParallaxLayer(3, "Mon vieux grimoire", "/images/project-4.png", "Lorem ipsum dolor sit amet, consectetur adipisicing elit.", "https://github.com/Xraww/Grimoire", 2, 4)}
-                    {createParallaxLayer(4, "Nina Carducci | Portfolio", "/images/project-5.png", "Lorem ipsum dolor sit amet, consectetur adipisicing elit.", "https://github.com/Xraww/Photographer", 3, 0)}
+                    {createParallaxLayer(3, "Mon vieux grimoire", "/images/project-4.png", "https://github.com/Xraww/Grimoire", 2, 4)}
+                    {createParallaxLayer(4, "Nina Carducci | Portfolio", "/images/project-5.png", "https://github.com/Xraww/Photographer", 3, 0)}
                 </Parallax>
             </div>
         </div>
